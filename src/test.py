425f823af806d618dd08f7efecb31b266eaf2ec6
Jesus Martin Zorrilla <jesusmarzor28@gmail.com>
2021-10-16 14:48:30 +0200

    Se crea el archivo para informar sobre el lenguaje y la entidad

--- src/test.py
+++ src/test.py
@@ -1,21 +1,10 @@
 import locale
 import calendar
 from datetime import *
-from Funciones import *
-from Electricidad import *
+from Calendario import *
 
 locale.setlocale(locale.LC_ALL,'es_ES.utf8')
-def obtener_datos():
-    # estos datos en vez de crear un objeto electricidad se cogerían de la base de datos pero ahora estamos probando que funciona
-    datos = {
-            'precio': obtener_precio(fecha),
-            'fecha': fecha,
-            'precio_estimado_manana': obtener_precio_estimado(fecha + timedelta(days=1)),
-            'porcentaje_de_subida_o_bajada':  obtener_porcentaje(fecha),
-            'dia_semana_actual': dia_de_la_semana(fecha),
-            'subida_precio': subida_precio(fecha)
-        }
-    return Electricidad(datos)
+
 
 print("¿De que dia quieres comprobar la electricidad?(xx)")
 dia = int(input())
@@ -26,21 +15,20 @@ ano = int(input())
 try:
     fecha = date(ano,mes,dia)
     print(calendar.month(ano,mes,2,1))
-    electricidad = obtener_datos()
-    print('Se ha buscado los datos de la electricidad del', electricidad.fecha)
-    print('El precio de la electricidad el dia', fecha, 'es', electricidad.precio)
-    if(electricidad.porcentaje_de_subida_o_bajada >= 0):
-        print('El porcentaje de subida es de', electricidad.porcentaje_de_subida_o_bajada)
+    calendario = Calendario(30,fecha) # 30 es un valor inventado, tendría que obtenerse de alguna manera el precio de la electricidad en esa fecha
+    print('Se ha buscado los datos de la electricidad del', calendario.obtener_fecha())
+    print('El precio de la electricidad el dia', fecha, 'es', calendario.obtener_precio())
+    if(calendario.obtener_porcentaje() >= 0):
+        print('El porcentaje de subida es de', calendario.obtener_porcentaje())
     else:
-        print('El porcentaje de bajada es de', electricidad.porcentaje_de_subida_o_bajada)    
-    print('El precio estimado para el dia', fecha + timedelta(days=1), 'es', electricidad.precio_estimado_manana)
-    print('El', fecha, 'es', electricidad.dia_semana_actual)
-    if(electricidad.subida_precio):
-        print('El', fecha, 'es fin de semana')
+        print('El porcentaje de bajada es de', calendario.obtener_porcentaje())    
+    print('El precio estimado para el dia', calendario.obtener_fecha() + timedelta(days=1), 'es', calendario.obtener_precio_estimado_manana())
+    print('El', calendario.obtener_fecha(), 'es', calendario.dia_de_la_semana())
+    if(calendario.fin_de_semana_o_fiesta):
+        print('El', calendario.obtener_fecha(), 'es fin de semana o fiesta')
     else:
-        print('El', fecha, 'no es fin de semana')
-    print('El dia más barato de esta semana es el',obtener_dia_de_la_semana_mas_barato())
-
+        print('El', calendario.obtener_fecha(), 'no es fin de semana o no es fiesta')
+    print('El dia más barato de esta semana es el', calendario.obtener_dia_de_la_semana_mas_barato())
 except:
     print('Escribe una fecha valida')
 
