03036394cdfcb225c1d590b7ca5533e4a407d88d
Jesus Martin Zorrilla <jesusmarzor28@gmail.com>
2021-10-21 11:21:09 +0200

    Se desarrollan las tareas joaquingv12#18 joaquingv12#20 para resolver la [HU1] joaquingv12#13

---
+++ src/Vehiculo.py
@@ -0,0 +1,48 @@
+from datetime import *
+import enum
+
+
+class TipoAveria(enum.Enum):
+    '''
+    Enumerado
+    ----------
+    '''
+    GOLPE = 1
+    RUEDA_PINCHADA = 2
+    
+
+class Vehiculo:
+    '''
+    Atributos
+    ----------
+    matricula : float
+        Matrícula del vehículo
+    tipo_averia : TipoAveria
+        Tipo de averia que tiene el vehiculo
+    fecha_de_llegada : Date
+        Fecha en la que el vehículo llega al taller
+    fecha_de_salida: Date
+        Fecha en la que el vehículo sale del taller
+    tiempo_estimado_reparacion : float
+        Tiempo estimado para la reparación del vehículo
+    '''
+    def __init__(self, matricula,  tipo_averia):
+        self.matricula = matricula
+        self.tipo_averia = tipo_averia
+        self.fecha_de_llegada = datetime.today()
+        self.fecha_de_salida = None
+    def obtener_matricula(self):
+        ''' Devuelve la matricula del vehículo '''
+        return self.matricula
+    def obtener_fecha_llegada(self):
+        ''' Devuelve la fecha de llegada del vehículo '''
+        return self.fecha_de_llegada
+    def obtener_tiempo_estimado_reparacion(self):
+        ''' Devuelve un tiempo estimado de reparación '''
+        return self.tiempo_estimado_reparacion
+    def obtener_fecha_de_salida(self):
+        ''' Devuelve la fecha de salida del vehículo '''
+        return self.fecha_de_salida
+    def fecha_de_sallida(self,fecha_de_salida):
+        ''' Añade la fecha de salida del vehículo '''
+        self.fecha_de_salida = fecha_de_salida
