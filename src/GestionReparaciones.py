33b695d5b0fb7c0b5644fe9c128b1fd43f7caf61
Jesus Martin Zorrilla <jesusmarzor28@gmail.com>
2021-10-21 11:26:23 +0200

    Se desarrolla la tarea joaquingv12#19 para resolver la [HU2] joaquingv12#14

---
+++ src/GestionReparaciones.py
@@ -0,0 +1,22 @@
+class GestionReparaciones:
+    '''
+    Atributos
+    ----------
+    listado_vehiculos : [Vehículo]
+        Listado de los vehículos que se pretenden reparar
+    puestos_de_reparacion : [int]
+        Número de puestos de reparación que hay en el taller
+    '''
+    def __init__(self, listado_vehiculos, puestos_de_reparacion):
+        self.listado_vehiculos = listado_vehiculos
+        self.puestos_de_reparacion = puestos_de_reparacion
+    
+    def distribuir_vehiculos():
+        ''' Distribuye los vehiculos en los puestos de reparación '''
+    def obtener_listado_distribucion():
+        ''' Devuelve el listado de los vehiculos ya distribuidos '''
+    def tiempo_estimado_terminar_reparaciones():
+        ''' Devolvera el tiempo estimado para terminar todas las reparaciones del día '''
+    def obtener_puestos_de_reparacion(self):
+        ''' Devuelve el número de puestos de reparación que hay en el taller '''
+        return self.puestos_de_reparacion
