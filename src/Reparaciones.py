c0cdbcc68735f76da740a5d70f33b28f6f4721f4
Joaquín García Venegas <joaquingv12@gmail.com>
2021-10-30 14:07:25 +0200

    Se añade validacion de datos, se resuelve #22 (closes #22)

--- src/Reparaciones.py
+++ src/Reparaciones.py
@@ -1,23 +1,42 @@
+from src.Vehiculo import *
+
 class Reparaciones:
     '''
+    Clase que gestiona las reparaciones de los vehículos del taller para cada día.
+
     Atributos
     ----------
-    listado_vehiculos : [Vehículo]
+    listado_vehiculos : [Vehiculo]
         Listado de los vehículos que se pretenden reparar
-    puestos_de_reparacion : [int]
+    puestos_de_reparacion : int
         Número de puestos de reparación que hay en el taller
+    lista_distribucion : [[Vehiculo]]
+        Lista con varias listas de vehículos, una para cada puesto del taller
     '''
+    
     def __init__(self, listado_vehiculos, puestos_de_reparacion):
         ''' Inicializa los atributos de la clase '''
+
+        #Validar los datos introducidos
+        for v in listado_vehiculos:
+            if type(v) != Vehiculo:
+                raise TypeError("El listado tiene que ser de objetos vehículos")
+        
+        if puestos_de_reparacion < 0:
+            raise ValueError("El número de puestos de reparación no puede ser negativo")
+
         self.listado_vehiculos = listado_vehiculos
         self.puestos_de_reparacion = puestos_de_reparacion
+        self.lista_distribucion = [[] for i in range(puestos_de_reparacion)]
     
+
     def distribuir_vehiculos():
         ''' Distribuye los vehiculos en los puestos de reparación '''
+    
     def obtener_listado_distribucion():
         ''' Devuelve el listado de los vehiculos ya distribuidos '''
+    
     def tiempo_estimado_terminar_reparaciones():
         ''' Devolvera el tiempo estimado para terminar todas las reparaciones del día '''
-    def obtener_puestos_de_reparacion(self):
-        ''' Devuelve el número de puestos de reparación que hay en el taller '''
-        return self.puestos_de_reparacion
+
+
--- src/Vehiculo.py
+++ src/Vehiculo.py
@@ -1,7 +1,6 @@
 from datetime import *
 import enum
 
-
 class TipoAveria(enum.Enum):
     '''
     Enumerado
@@ -9,13 +8,17 @@ class TipoAveria(enum.Enum):
     '''
     GOLPE = 1
     RUEDA_PINCHADA = 2
-    
+    LUNA_ROTA = 3
+    MOTOR_AVERIADO = 4
+    BATERIA_GASTADA = 5
 
 class Vehiculo:
     '''
+    Clase que contiene la información de cada vehículo averiado.
+
     Atributos
     ----------
-    matricula : float
+    matricula : str
         Matrícula del vehículo
     tipo_averia : TipoAveria
         Tipo de averia que tiene el vehiculo
@@ -23,36 +26,41 @@ class Vehiculo:
         Fecha en la que el vehículo llega al taller
     fecha_de_salida: Date
         Fecha en la que el vehículo sale del taller
-    tiempo_estimado_reparacion : float
-        Tiempo estimado para la reparación del vehículo
+    tiempo_estimado_reparacion : int
+        Tiempo estimado (minutos) para la reparación del vehículo
     reparado : boolean
         true si está reparado, false si está averiado
     '''
-    def __init__(self, matricula,  tipo_averia):
+    def __init__(self, matricula, tipo_averia, tiempo_estimado_reparacion):
         ''' Inicializa los atributos de la clase '''
+
+        #Validar los datos introducidos
+        if type(matricula) != str or len(matricula) != 7:
+            raise TypeError("Matrícula incorrecta")
+
+        if type(tipo_averia) != TipoAveria:
+            raise TypeError("Tipo de avería no es correcto")
+
+        if type(tiempo_estimado_reparacion) != int:
+            raise TypeError("Tiempo estimado de reparación tiene que ser un entero")
+        
+        if tiempo_estimado_reparacion <= 0:
+            raise ValueError("El tiempo estimado de reparación tiene que ser mayor a 0")
+
         self.matricula = matricula
         self.tipo_averia = tipo_averia
-        self.fecha_de_llegada = datetime.today()
+        self.tiempo_estimado_reparacion = tiempo_estimado_reparacion
+        self.fecha_de_llegada = date.today()
         self.fecha_de_salida = None
         self.reparado = False
-    def obtener_matricula(self):
-        ''' Devuelve la matricula del vehículo '''
-        return self.matricula
-    def obtener_fecha_llegada(self):
-        ''' Devuelve la fecha de llegada del vehículo '''
-        return self.fecha_de_llegada
-    def obtener_tiempo_estimado_reparacion(self):
-        ''' Devuelve un tiempo estimado de reparación '''
-        return self.tiempo_estimado_reparacion
-    def obtener_fecha_de_salida(self):
-        ''' Devuelve la fecha de salida del vehículo '''
-        return self.fecha_de_salida
-    def fecha_de_salida(self,fecha_de_salida):
+ 
+
+    def aniadir_fecha_salida(self,fecha_de_salida):
         ''' Añade la fecha de salida del vehículo '''
+        if not isinstance(fecha_de_salida,date):
+            raise TypeError("El formato de la fecha de salida no es correcto")
         self.fecha_de_salida = fecha_de_salida
-    def obtener__reparado(self):
-        ''' Devuelve si el vehículo está reparado o no '''
-        return self.reparado
-    def reparado(self,reparado):
+    
+    def cambiar_estado_reparacion(self, reparado):
         ''' Cambia el estado de reparación del vehículo '''
         self.reparado = reparado
