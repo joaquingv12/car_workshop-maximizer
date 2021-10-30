c2c1cc5bea9ef8c10397d2fe2740dee35d4345ac
Joaquín García Venegas <joaquingv12@gmail.com>
2021-10-30 16:19:46 +0200

    Se crean tests para testear las clases Vehiculo y Reparaciones

---
+++ test/test_reparaciones.py
@@ -0,0 +1,25 @@
+import pytest
+from src.Reparaciones import *
+from src.Vehiculo import *
+
+@pytest.fixture
+def reparaciones():
+    v1 = Vehiculo('1234ABC', TipoAveria.RUEDA_PINCHADA, 10)
+    v2 = Vehiculo('2345ABD', TipoAveria.GOLPE, 30)
+    v3 = Vehiculo('2345ACD', TipoAveria.MOTOR_AVERIADO, 60)
+    v4 = Vehiculo('4345ACD', TipoAveria.BATERIA_GASTADA, 5)
+    listado_vehiculos = [v1, v2, v3, v4]
+    reparaciones = Reparaciones(listado_vehiculos, 3)    
+    return reparaciones
+
+def test_lista_distribucion_vacia_al_crearlo(reparaciones):
+    assert len(reparaciones.lista_distribucion) == reparaciones.puestos_de_reparacion
+    for i in reparaciones.lista_distribucion:
+        assert len(i) == 0
+
+def test_añadir_nuevo_vehiculo(reparaciones):
+    nuevo_vehiculo = Vehiculo('4231CDE', TipoAveria.BATERIA_GASTADA, 5)
+    reparaciones.añadir_nuevo_vehiculo(nuevo_vehiculo)
+    assert len(reparaciones.listado_vehiculos) == 5
+
+
---
+++ test/test_vehiculos.py
@@ -0,0 +1,27 @@
+import pytest
+from src.Vehiculo import *
+
+@pytest.fixture
+def vehiculo():
+    vehiculo = Vehiculo('1234ABC', TipoAveria.RUEDA_PINCHADA, 5)
+    return vehiculo
+
+def test_tiene_fecha_de_llegada_correcta_al_crearlo(vehiculo):
+    assert vehiculo.fecha_de_llegada <= date.today()
+
+def test_vehiculo_averiado_al_crearlo(vehiculo):
+    assert vehiculo.reparado == False
+
+def test_cambiar_a_estado_reparado(vehiculo):
+    vehiculo.cambiar_estado_reparacion(True)
+    assert vehiculo.reparado == True
+
+def test_cambiar_a_estado_no_reparado(vehiculo):
+    vehiculo.cambiar_estado_reparacion(False)
+    assert vehiculo.reparado == False
+
+def test_cambiar_fecha_salida(vehiculo):
+    vehiculo.aniadir_fecha_salida(date.today())
+    assert vehiculo.fecha_de_salida == date.today()
+
+
