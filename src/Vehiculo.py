dce7a5cb3c6d00f9380e91602e0516cca4326765
Joaquín García Venegas <joaquingv12@gmail.com>
2021-11-05 13:56:53 +0100

    Se permite añadir varias averias, resuelve #25 (closes #25)

--- src/Vehiculo.py
+++ src/Vehiculo.py
@@ -20,8 +20,8 @@ class Vehiculo:
     ----------
     matricula : str
         Matrícula del vehículo
-    tipo_averia : TipoAveria
-        Tipo de averia que tiene el vehiculo
+    tipo_averias : [TipoAveria]
+        Listado con el tipo de averias que tiene el vehiculo
     fecha_de_llegada : Date
         Fecha en la que el vehículo llega al taller
     fecha_de_salida: Date
@@ -31,16 +31,20 @@ class Vehiculo:
     reparado : boolean
         true si está reparado, false si está averiado
     '''
-    def __init__(self, matricula, tipo_averia, tiempo_estimado_reparacion):
+    def __init__(self, matricula, listado_averias, tiempo_estimado_reparacion):
         ''' Inicializa los atributos de la clase '''
 
         #Validar los datos introducidos
-        if type(matricula) != str or len(matricula) != 7:
+        if type(matricula) != str or len(matricula) <= 3: #La mayoría de las matrículas tienen al menos 4 caracteres
             raise TypeError("Matrícula incorrecta")
 
-        if type(tipo_averia) != TipoAveria:
-            raise TypeError("Tipo de avería no es correcto")
-
+        if type(listado_averias) != list:
+            listado_averias = [listado_averias]
+        
+        for averia in listado_averias:
+            if type(averia) != TipoAveria:
+                listado_averias.remove(averia) #No añadir las averías que no son correctas
+        
         if type(tiempo_estimado_reparacion) != int:
             raise TypeError("Tiempo estimado de reparación tiene que ser un entero")
         
@@ -48,7 +52,7 @@ class Vehiculo:
             raise ValueError("El tiempo estimado de reparación tiene que ser mayor a 0")
 
         self.matricula = matricula
-        self.tipo_averia = tipo_averia
+        self.listado_averias = listado_averias
         self.tiempo_estimado_reparacion = tiempo_estimado_reparacion
         self.fecha_de_llegada = date.today()
         self.fecha_de_salida = None
@@ -60,6 +64,12 @@ class Vehiculo:
         if not isinstance(fecha_de_salida,date):
             raise TypeError("El formato de la fecha de salida no es correcto")
         self.fecha_de_salida = fecha_de_salida
+
+    def añadir_nueva_averia(self,nueva_averia):
+        ''' Añade una nueva avería al vehículo '''
+        if type(nueva_averia) != TipoAveria:
+            raise TypeError("El tipo de avería no es correcto")
+        self.listado_averias.append(nueva_averia)
     
     def cambiar_estado_reparacion(self, reparado):
         ''' Cambia el estado de reparación del vehículo '''
